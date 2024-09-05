197b076e5ddd8bd8a64807375925b6e2446a24f9
Say something <163742930+Raks-kmt@users.noreply.github.com>
2024-09-05 23:41:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "28526237"))
+API_HASH = getenv("API_HASH", "936db76a74f9a52cfb2cea8a62e4c20e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7397842341:AAFIioQE_xJKcfoLVCMtUjf04_2ZT7SFpns")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6486192717").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://prisarax:WxuKANYzL5CPyY7d@cluster0.jxlwo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002204974717")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "2204974717"))
